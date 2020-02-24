6ec1d5295806a60f72bdad31b91d9d10c2cfd869
Jeremy Perry <jeremy.m.perry@gmail.com>
2020-02-24 17:23:15 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create(first_name: 'John', last_name: 'Perry')
+Student.create(first_name: 'Sally', last_name: 'Johnson')
+Student.create(first_name: 'Diana', last_name: 'Mkrtchyan')
+Student.create(first_name: 'Able', last_name: 'Thomas')
+Student.create(first_name: 'Tiffany', last_name: 'Flores')
+Student.create(first_name: 'Josiah', last_name: 'Perry')
